e4772a3f7cd720822643b62751f315b191253d5e
Hyun <itsmine096@gmail.com>
2025-04-02 20:48:28 +0900

    [feat] server - 전역 예외 처리 기능 추가

---
+++ server/src/main/java/com/hello/chatting/global/error/ErrorCode.java
@@ -0,0 +1,25 @@
+package com.hello.chatting.global.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+	// Common
+	INVALID_INPUT_VALUE(400, " Invalid Input Value"),
+	METHOD_NOT_ALLOWED(405, " Invalid Input Value"),
+	ENTITY_NOT_FOUND(400, " Entity Not Found"),
+	INTERNAL_SERVER_ERROR(500, "Server Error"),
+	INVALID_TYPE_VALUE(400, " Invalid Type Value"),
+
+	;
+	private final int status;
+	private final String message;
+
+	ErrorCode(int status, String message) {
+		this.status = status;
+		this.message = message;
+	}
+}
